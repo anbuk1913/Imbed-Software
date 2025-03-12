33aae674f2e5388f909fc78909de0b5046db99bc
anbuk143 <anbukumar1913@gmail.com>
2025-03-12 17:49:10 +0530

    Google Auth

--- config/passport.js
+++ config/passport.js
@@ -2,10 +2,10 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://www.imbedsoftware.shop/auth/google/callback'
-  }, (accessToken, refreshToken, profile, done) => {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: 'https://www.imbedsoftware.shop/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
 }));
 
